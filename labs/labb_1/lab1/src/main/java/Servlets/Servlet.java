972d7162ab6607c93a07199f76510ccf85820e06
Darynnen <darynaboruk@gmail.com>
2023-01-30 01:58:26 +0200

    +

---
+++ labs/labb_1/lab1/src/main/java/Servlets/Servlet.java
@@ -0,0 +1,25 @@
+package Servlets;
+
+import Entities.Entity;
+import com.google.gson.Gson;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet("/getdata")
+public class Servlet extends HttpServlet {
+
+    @Override
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        Entity entity = new Entity("GARMIN MARQ COMMANDER", 2950);
+
+        PrintWriter out = response.getWriter();
+        response.setContentType("application/json");
+
+        out.println("["+entity+"]");
+    }
+}
