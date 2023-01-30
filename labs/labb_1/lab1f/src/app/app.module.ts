1d5604f775e546a043cf81c6d1d0fcfe03b40013
Darynnen <darynaboruk@gmail.com>
2023-01-30 19:47:40 +0200

    +

--- labs/labb_1/lab1f/src/app/app.module.ts
+++ labs/labb_1/lab1f/src/app/app.module.ts
@@ -5,11 +5,13 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
 import { HeaderComponent } from './header/header.component';
+import { AboutComponent } from './about/about.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
