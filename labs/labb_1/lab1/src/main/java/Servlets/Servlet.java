f3cb64e4a44b7f46ce459689b0436f74ff874df2
Darynnen <darynaboruk@gmail.com>
2023-01-30 13:47:35 +0200

    +

--- labs/labb_1/lab1/src/main/java/Servlets/Servlet.java
+++ labs/labb_1/lab1/src/main/java/Servlets/Servlet.java
@@ -10,7 +10,7 @@ import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
 
-@WebServlet("/getdata")
+@WebServlet("/servlet")
 public class Servlet extends HttpServlet {
 
     @Override
